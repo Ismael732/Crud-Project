5640aa46a69a4ba036acfdee344032023a6bd397
iago mateus do amaral penz <160060594+Ghost2548@users.noreply.github.com>
2024-09-17 20:48:45 -0300

    Crud

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 
-class User{
+class User {
   final String? id;
   final String? name;
   final String? email;
@@ -8,9 +8,8 @@ class User{
 
   const User({
     this.id,
-    @required this.name,
-    @required this.email,
-    @required this.avatarUrl,
-
+     this.name,
+     this.email,
+     this.avatarUrl,
   });
 }
