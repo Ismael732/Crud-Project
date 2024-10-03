b2f6b70f8d6ab92da42d5871744ba3c04624fe7b
QI <michelsantiago38@gmail.com>
2024-10-03 19:44:15 -0300

    Crud

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/material.dart';
+import 'package:crud/provider/users.dart';
+import 'package:crud/routes/app_routes.dart';
+import 'package:crud/views/user_form.dart';
 import 'package:crud/views/user_list.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,16 +12,26 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Users(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        routes: {
+          AppRoutes.home: (_) => UserList(),
+          AppRoutes.user_form: (_) => UserForm()
+        },
       ),
-      home: UserList(),
     );
   }
 }
